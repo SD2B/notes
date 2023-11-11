a63d02690db968ec14ced53251e9dffb07caa5e3
Sanoop Das <lonewolf.sddb@gmail.com>
2023-11-11 13:33:16 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
